ee00a3924157d2ae3e3cf8919bd0f8036618b42f
MIKA ARAI <mika8500@yk.commufa.jp>
2024-01-06 22:58:32 +0900

    ログインページ　途中

--- blog-with-react-and-firebase/src/components/Login.js
+++ blog-with-react-and-firebase/src/components/Login.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import { auth, provider } from "../firebase";
+import { signInWithPopup } from "firebase/auth";
 
 function Login() {
+  const loginInWithGoogle = () => {
+    // Googleでログイン
+    signInWithPopup(auth, provider).then((result)=> {
+      console.log(result);
+
+    });
+  };
+
   return (
-    <div>Login</div>
+    <div>
+      <p>ログインして始める</p>
+      <button onClick={loginInWithGoogle}>Googleでログイン</button>
+    </div>
   )
 }
 
