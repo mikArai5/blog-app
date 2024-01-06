136e8d67be261c541404249e09d6adf54c975d08
MIKA ARAI <mika8500@yk.commufa.jp>
2024-01-06 22:00:09 +0900

    Home コンポーネント作成

--- blog-with-react-and-firebase/src/App.js
+++ blog-with-react-and-firebase/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"; 
+import Home from './components/Home';
+
 
 function App() {
   return (
-    <div>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
