49c0e8e33e338ee2b11586be5f31abdeca091039
MIKA ARAI <mika8500@yk.commufa.jp>
2024-01-06 22:07:30 +0900

    createpost/login/logout　コンポーネント作成

--- blog-with-react-and-firebase/src/App.js
+++ blog-with-react-and-firebase/src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"; 
 import Home from './components/Home';
+import CreatePost from './components/CreatePost';
+import Login from './components/Login';
+import Logout from './components/Logout';
 
 
 function App() {
@@ -8,6 +11,9 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/createpost" element={<CreatePost />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/logout" element={<Logout />}></Route>
       </Routes>
     </Router>
   );
