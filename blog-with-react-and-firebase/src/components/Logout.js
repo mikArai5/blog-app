90f9b59144ac3ff83770c3efaef45f1a2674f271
MIKA ARAI <mika8500@yk.commufa.jp>
2024-01-08 21:11:04 +0900

    ターミナル  エラー

--- blog-with-react-and-firebase/src/components/Logout.js
+++ blog-with-react-and-firebase/src/components/Logout.js
@@ -1,6 +1,6 @@
-import { signInWithPopup, signOut } from "firebase/auth";
+import { signOut } from "firebase/auth";
 import React from 'react';
-import { auth, provider } from "../firebase";
+import { auth } from "../firebase";
 import { useNavigate } from "react-router-dom";
 
 function Logout({setIsAuth}) {
