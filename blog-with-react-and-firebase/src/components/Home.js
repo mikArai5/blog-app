984a86d45f0892cd31a239cb16b83484a9a22c51
MIKA ARAI <mika8500@yk.commufa.jp>
2024-01-08 20:41:35 +0900

    記事投稿　実装

--- blog-with-react-and-firebase/src/components/Home.js
+++ blog-with-react-and-firebase/src/components/Home.js
@@ -1,32 +1,36 @@
 import { collection, getDocs } from "firebase/firestore";
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { db } from "../firebase";
 import "./Home.css";
 
 function Home() {
+  const [postList, setPostList] = useState([]);
 
   useEffect(() => {
     const gestPosts = async () => {
       const data = await getDocs(collection(db,"posts"));
       // ▼取り出す階層がすごく深い
       // console.log(data.docs.map((doc) => ({...doc.data(), id: doc.id })));
+      setPostList(data.docs.map((doc) => ({...doc.data(), id: doc.id })));
     };
     gestPosts();
   }, []);
 
   return <div className='homePage'>
-    <div className='postContents'>
-      <div className='postHeader'>
-        <h1>タイトル</h1>
+    {postList.map((post) => {
+      return (
+        <div className='postContents' key={post.id}>
+        <div className='postHeader'>
+          <h1>{post.title}</h1>
+        </div>
+        <div className='postTextContainer'>{post.postsText}</div>
+        <div className='nameAndDeleteButton'>
+          <h3>@{post.author.username}</h3>
+          <button>削除</button>
+        </div>
       </div>
-      <div className='postTextContainer'>
-        React学習中です。でも難しすぎてmshdhjfdsfjsjbfsdkbかjsdんあslkdslkdんcjksdんjcんsdjbchsdbvldjfkんvkd
-      </div>
-      <div className='nameAndDeleteButton'>
-        <h3>@もにょもにょ</h3>
-        <button>削除</button>
-      </div>
-    </div>
+      );
+    })}
   </div>
 }
 
